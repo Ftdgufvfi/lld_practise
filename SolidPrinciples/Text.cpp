d2d7b43714ac319185fb197fea49390b23b400ca
Chevvuri Karthik <krtk85@gmail.com>
2026-07-25 22:13:57 +0530

    Update workspace files (2026-07-25 22:13:57)

---
+++ SolidPrinciples/Text.cpp
@@ -0,0 +1,13 @@
+// Why we need SOLID principles?
+//
+// Scalability: Adding new features is straightforward.
+//
+// Maintainability: Changes in one part have minimal impact on other parts.
+//
+// Testability: Decoupled designs make unit testing easier.
+//
+// Readability: Clear separation of concerns improves code comprehension.
+
+int main() {
+	return 0;
+}
